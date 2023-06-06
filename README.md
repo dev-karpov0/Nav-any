43446b8655644dda5c02cb038549dba9ec49bd21
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2023-06-06 10:30:16 +1000

    Parse map

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Nav-any
 Indoor-navigator
+
+Идея универсального навигатора внутри зданий описана [здесь](https://habr.com/ru/sandbox/171460/).
+
+На этом сайте доступны:
+  * [Навигатор по ВВГУ](https://lapkin25.github.io/Nav-any/VVSU)
+  * [Навигатор по ДВФУ](https://lapkin25.github.io/Nav-any/FEFU)
--- read_plan.js
+++ read_plan.js
@@ -1,4 +1,5 @@
-// String planXML needs to be defined
+// преобразуем строку с XML-файлом в дерево XML-документа
+// String planXML must be defined
 let xmlDoc;
 if (window.DOMParser)
 {
@@ -15,7 +16,7 @@ else  // Internet Explorer
 /*
     Структура данных "План здания":
        points - массив пунктов (пункт: этаж, id, название, тип пункта)
-       paths - массив путей (...)
+       paths - массив путей перемещения (путь: тип пути, направление перемещения)
        point_by_id - ассоциативный массив (ключ - строковый id, значение - индекс пункта)
 */
 
@@ -27,30 +28,70 @@ let plan = {
     elevator_by_id: new Map()
 };
 
-function add_point ()
-{
+const motionDir = { UP: "up", RIGHT: "right", DOWN: "down", LEFT: "left" };
 
+// добавляет точку к плану
+function add_point (point)
+{
+    plan.points.push(point);
+    let point_index = plan.points.length - 1;
+    plan.point_by_id.set(point.id, point_index);
 }
 
+// строит структуру данных "План здания"
 function read_plan ()
 {
     let root = xmlDoc.getElementsByTagName("Plan")[0];
-    //console.log(root);
     for (const plan_child of root.childNodes) {
-       //if (plan_child.nodeType != 1)   // this is not tag
-       //    continue;
-        if (plan_child.tagName == 'Exit') {
+        if (plan_child.tagName == "Exit") {
             let point_exit = {
                 floor: plan_child.attributes["floor"].nodeValue,
                 id: plan_child.attributes["id"].nodeValue,
                 name: ("name" in plan_child.attributes ? plan_child.attributes["name"].nodeValue : "Выход"),
-                point_type: "Exit",
+                type: "Exit",
             };
             add_point(point_exit);
-           
-            console.log(point_exit);
+        }
+        else if (plan_child.tagName == "Floor") {
+            floor_num = parseInt(plan_child.attributes["num"].nodeValue);
+            for (floor_child of plan_child.childNodes) {
+                if (floor_child.tagName == "Path") {
+                    let dir_str = floor_child.attributes["dir"];
+                    let dir = function(dir_str) {
+                        switch (dir_str) {
+                            case "up":
+                                return UP;
+                                break;
+                            case "right":
+                                return RIGHT;
+                                break;
+                            case "down":
+                                return DOWN;
+                                break;
+                            case "left":
+                                return LEFT;
+                                break;
+                        }
+                    }
+                    let path = {
+                        type: floor_child.attributes["type"],
+                        dir: dir,                                               
+                    };
+                    // add path
+                    plan.paths.push(path);
+                    let path_index = plan.paths.length - 1;
+                    // add starting point to path
+                    let point_index = get_point_by_id(floor_child.attributes["start"], floor_num);
+                    add_point_to_path(point_index, path_index);
+                    // read path
+                    read_path(floor_child, path_index, floor_num);
+                }
+                else
+                    process_xml_node(floor_child, -1, floor_num);
+            }
         }
     }
 }
 
 read_plan();
+// console.log(plan);
